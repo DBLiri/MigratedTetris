19914e751480e3b3e7f09c3583936dacb0977c43
DBLiri <Dylanbliriano@gmail.com>
2026-09-20 19:35:20 -0400

    Fixing editor files weird stuff

---
+++ SUPER DUPER TETRIS/Source/SUPER_DUPER_TETRIS/MyInitalClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SUPER_DUPER_TETRIS_API MyInitalClass
+{
+public:
+	MyInitalClass();
+	~MyInitalClass();
+};
